c0685b684d440d38eca85204b610d40112c57f20
Jaghaimo <1764586+jaghaimo@users.noreply.github.com>
2022-10-26 18:56:34 +0100

    Adjust code completion preview

--- dot_config/lvim/config.lua
+++ dot_config/lvim/config.lua
@@ -12,6 +12,9 @@ an executable
 lvim.log.level = "warn"
 lvim.format_on_save = true
 lvim.colorscheme = "tokyonight"
+lvim.builtin.cmp.completion = {
+  completeopt = "menu,menuone,preview"
+}
 -- lvim.colorscheme = "gruvbox"
 -- vim.o.background = "dark"
 -- to disable icons and use a minimalist setup, uncomment the following
@@ -20,6 +23,7 @@ lvim.colorscheme = "tokyonight"
 -- keymappings [view all the defaults by pressing <leader>Lk]
 lvim.leader = "space"
 -- add your own keymapping
+lvim.keys.normal_mode["<C-Space>"] = "ea"
 lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
 lvim.keys.normal_mode["<S-l>"] = ":BufferLineCycleNext<CR>"
 lvim.keys.normal_mode["<S-h>"] = ":BufferLineCyclePrev<CR>"
