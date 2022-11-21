053596206e7f17e7bad1a6539d96a65257c1c919
Jaghaimo <1764586+jaghaimo@users.noreply.github.com>
2022-11-21 08:56:02 +0000

    LunarVim updates and lazygit script

--- dot_config/lvim/config.lua
+++ dot_config/lvim/config.lua
@@ -171,6 +171,7 @@ formatters.setup {
   { command = "black", filetypes = { "python" } },
   { command = "csharpier", filetypes = { "cs" } },
   { command = "isort", filetypes = { "python" } },
+  { command = "phpcsfixer", filetypes = { "php" }, args = { "--rules=@PhpCsFixer", "fix", "$FILENAME" } },
   -- {
   --   -- each formatter accepts a list of options identical to https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md#Configuration
   --   command = "prettier",
@@ -185,19 +186,9 @@ formatters.setup {
 -- -- set additional linters
 local linters = require "lvim.lsp.null-ls.linters"
 linters.setup {
+  { command = "codespell" },
+  { command = "cspell", filetypes = { "md" } },
   { command = "flake8", filetypes = { "python" } },
-  --   {
-  --     -- each linter accepts a list of options identical to https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md#Configuration
-  --     command = "shellcheck",
-  --     ---@usage arguments to pass to the formatter
-  --     -- these cannot contain whitespaces, options such as `--line-width 80` become either `{'--line-width', '80'}` or `{'--line-width=80'}`
-  --     extra_args = { "--severity", "warning" },
-  --   },
-  --   {
-  --     command = "codespell",
-  --     ---@usage specify which filetypes to enable. By default a providers will attach to all the filetypes it supports.
-  --     filetypes = { "javascript", "python" },
-  --   },
 }
 
 -- Additional Plugins
@@ -294,13 +285,15 @@ lvim.plugins = {
   -- Support for .editorconfig
   { 'gpanders/editorconfig.nvim' },
   -- Highlight arguments throught the function
-  -- {
-  --   'm-demare/hlargs.nvim',
-  --   requires = { 'nvim-treesitter/nvim-treesitter' },
-  --   config = function()
-  --     require('hlargs').setup()
-  --   end,
-  -- },
+  {
+    'm-demare/hlargs.nvim',
+    requires = { 'nvim-treesitter/nvim-treesitter' },
+    config = function()
+      require('hlargs').setup({
+        highlight = { italic = true }
+      })
+    end,
+  },
   -- Clipboard manager
   {
     "AckslD/nvim-neoclip.lua",
@@ -365,7 +358,9 @@ lvim.plugins = {
   {
     "ray-x/lsp_signature.nvim",
     event = "BufRead",
-    config = function() require "lsp_signature".on_attach() end,
+    config = function()
+      require "lsp_signature".on_attach()
+    end,
   },
   -- When opening a file, put cursor at the last place it was
   {
