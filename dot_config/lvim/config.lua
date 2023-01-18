c6486633a5a28f7282c6f3dc81854f98271d15e2
Jaghaimo <1764586+jaghaimo@users.noreply.github.com>
2023-01-18 14:08:22 +0000

    Change neotest to nvim-dap-python and cleanup config

--- dot_config/lvim/config.lua
+++ dot_config/lvim/config.lua
@@ -1,86 +1,23 @@
---[[
-lvim is the global options object
-
-Linters should be
-filled in as strings with either
-a global executable or a path to
-an executable
-]]
--- THESE ARE EXAMPLE CONFIGS FEEL FREE TO CHANGE TO WHATEVER YOU WANT
-
 -- general
 lvim.log.level = "warn"
 lvim.format_on_save = true
 lvim.colorscheme = "tokyonight-storm"
 lvim.builtin.autopairs.active = false
--- lvim.builtin.cmp.completion = {
---   completeopt = "menu,menuone,preview"
--- }
--- lvim.colorscheme = "gruvbox"
--- vim.o.background = "dark"
--- to disable icons and use a minimalist setup, uncomment the following
--- lvim.use_icons = false
 
 -- keymappings [view all the defaults by pressing <leader>Lk]
 lvim.leader = "space"
+
 -- add your own keymapping
 lvim.keys.normal_mode["<C-Space>"] = "ea"
 lvim.keys.normal_mode["<C-s>"] = ":w<CR>"
 lvim.keys.normal_mode["<S-l>"] = ":BufferLineCycleNext<CR>"
 lvim.keys.normal_mode["<S-h>"] = ":BufferLineCyclePrev<CR>"
 lvim.keys.normal_mode["Y"] = "y$"
--- unmap a default keymapping
--- vim.keymap.del("n", "<C-Up>")
--- override a default keymapping
--- lvim.keys.normal_mode["<C-q>"] = ":q<cr>" -- or vim.keymap.set("n", "<C-q>", ":q<cr>" )
-lvim.lsp.buffer_mappings.normal_mode["gr"] = { "<cmd>Trouble lsp_references<cr>" }
-
--- substitute.nvim
-vim.keymap.set("n", "t", "<cmd>lua require('substitute').operator()<cr>", { noremap = true })
-vim.keymap.set("n", "tt", "<cmd>lua require('substitute').line()<cr>", { noremap = true })
-vim.keymap.set("n", "T", "<cmd>lua require('substitute').eol()<cr>", { noremap = true })
-vim.keymap.set("x", "t", "<cmd>lua require('substitute').visual()<cr>", { noremap = true })
-vim.keymap.set("n", "tx", "<cmd>lua require('substitute.exchange').operator()<cr>", { noremap = true })
-vim.keymap.set("n", "txx", "<cmd>lua require('substitute.exchange').line()<cr>", { noremap = true })
-vim.keymap.set("x", "X", "<cmd>lua require('substitute.exchange').visual()<cr>", { noremap = true })
-vim.keymap.set("n", "txc", "<cmd>lua require('substitute.exchange').cancel()<cr>", { noremap = true })
-
--- Change Telescope navigation to use j and k for navigation and n and p for history in both input and normal mode.
--- we use protected-mode (pcall) just in case the plugin wasn't loaded yet.
--- local _, actions = pcall(require, "telescope.actions")
--- lvim.builtin.telescope.defaults.mappings = {
---   -- for input mode
---   i = {
---     ["<C-j>"] = actions.move_selection_next,
---     ["<C-k>"] = actions.move_selection_previous,
---     ["<C-n>"] = actions.cycle_history_next,
---     ["<C-p>"] = actions.cycle_history_prev,
---   },
---   -- for normal mode
---   n = {
---     ["<C-j>"] = actions.move_selection_next,
---     ["<C-k>"] = actions.move_selection_previous,
---   },
--- }
 
 -- Use which-key to add extra bindings with the leader-key prefix
 lvim.builtin.which_key.mappings["b"]["c"] = { "<cmd>%bd <bar> e# <bar> bd# <CR>", "Close others" }
 lvim.builtin.which_key.mappings["n"] = { "<cmd>Telescope neoclip<CR>", "Neoclip" }
-lvim.builtin.which_key.mappings["r"] = {
-  name = "+Trouble",
-  r = { "<cmd>Trouble lsp_references<cr>", "References" },
-  f = { "<cmd>Trouble lsp_definitions<cr>", "Definitions" },
-  d = { "<cmd>Trouble document_diagnostics<cr>", "Diagnostics" },
-  q = { "<cmd>Trouble quickfix<cr>", "QuickFix" },
-  l = { "<cmd>Trouble loclist<cr>", "LocationList" },
-  w = { "<cmd>Trouble workspace_diagnostics<cr>", "Workspace Diagnostics" },
-}
-lvim.builtin.which_key.mappings["t"] = {
-  name = "+Testing",
-  t = { "<cmd>lua require(\"neotest\").run.run()<CR>", "Nearest test" },
-  f = { "<cmd>lua require(\"neotest\").run.run(vim.fn.expand(\"%\"))<CR>", "Whole file" },
-  d = { "<cmd>lua require(\"neotest\").run.run({strategy = \"dap\"})<CR>", "Debug nearest test" },
-}
+lvim.builtin.which_key.mappings["t"] = { "<cmd>lua require('dap-python').test_method()<CR>", "Test this" }
 
 -- After changing plugin config exit and reopen LunarVim, Run :PackerCompile
 lvim.builtin.alpha.active = true
@@ -108,67 +45,7 @@ lvim.builtin.treesitter.ensure_installed = {
 lvim.builtin.treesitter.ignore_install = { "haskell" }
 lvim.builtin.treesitter.highlight.enable = true
 
--- generic LSP settings
-
--- -- make sure server will always be installed even if the server is in skipped_servers list
--- lvim.lsp.installer.setup.ensure_installed = {
---     "sumneko_lua",
---     "jsonls",
--- }
--- -- change UI setting of `LspInstallInfo`
--- -- see <https://github.com/williamboman/nvim-lsp-installer#default-configuration>
--- lvim.lsp.installer.setup.ui.check_outdated_servers_on_open = false
--- lvim.lsp.installer.setup.ui.border = "rounded"
--- lvim.lsp.installer.setup.ui.keymaps = {
---     uninstall_server = "d",
---     toggle_server_expand = "o",
--- }
-
--- ---@usage disable automatic installation of servers
--- lvim.lsp.installer.setup.automatic_installation = false
-
--- ---configure a server manually. !!Requires `:LvimCacheReset` to take effect!!
--- ---see the full default list `:lua print(vim.inspect(lvim.lsp.automatic_configuration.skipped_servers))`
--- check the lspconfig documentation for a list of all possible options
--- Use pylsp instead of pyright
--- vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "pyright" })
--- local lspconfig = require "lspconfig"
--- lspconfig.pylsp.setup {
--- on_attach = on_attach,
--- capabilities = capabilities,
--- settings = {
---   pylsp = {
---     configurationSources = { "flake8" },
---     plugins = {
---       autopep8 = { enabled = false },
---       flake8 = { enabled = true },
---       mccabe = { enabled = false },
---       pyflakes = { enabled = false },
---       pycodestyle = { enabled = false },
---       pylint = { enabled = false },
---       yapf = { enabled = false },
---     },
---   }
--- }
--- }
-
--- ---remove a server from the skipped list, e.g. eslint, or emmet_ls. !!Requires `:LvimCacheReset` to take effect!!
--- ---`:LvimInfo` lists which server(s) are skipped for the current filetype
--- lvim.lsp.automatic_configuration.skipped_servers = vim.tbl_filter(function(server)
---   return server ~= "emmet_ls"
--- end, lvim.lsp.automatic_configuration.skipped_servers)
-
--- -- you can set a custom on_attach function that will be used for all the language servers
--- -- See <https://github.com/neovim/nvim-lspconfig#keybindings-and-completion>
--- lvim.lsp.on_attach_callback = function(client, bufnr)
---   local function buf_set_option(...)
---     vim.api.nvim_buf_set_option(bufnr, ...)
---   end
---   --Enable completion triggered by <c-x><c-o>
---   buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
--- end
-
--- -- telescope
+-- telescope full view
 for key, _ in pairs(lvim.builtin.telescope.pickers) do
   if key ~= "planets" then
     lvim.builtin.telescope.pickers[key].previewer = nil
@@ -177,30 +54,22 @@ for key, _ in pairs(lvim.builtin.telescope.pickers) do
   end
 end
 
--- -- set a formatter, this will override the language server formatting capabilities (if it exists)
+-- set a formatter, this will override the language server formatting capabilities (if it exists)
 local formatters = require "lvim.lsp.null-ls.formatters"
 formatters.setup {
   { command = "black", filetypes = { "python" } },
   { command = "csharpier", filetypes = { "cs" } },
   { command = "isort", filetypes = { "python" } },
-  -- {
-  --   -- each formatter accepts a list of options identical to https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md#Configuration
-  --   command = "prettier",
-  --   ---@usage arguments to pass to the formatter
-  --   -- these cannot contain whitespaces, options such as `--line-width 80` become either `{'--line-width', '80'}` or `{'--line-width=80'}`
-  --   extra_args = { "--print-with", "100" },
-  --   ---@usage specify which filetypes to enable. By default a providers will attach to all the filetypes it supports.
-  --   filetypes = { "typescript", "typescriptreact" },
-  -- },
 }
 
+-- fix php-cs-fixer
 local null_ls = require("null-ls")
 null_ls.register {
   null_ls.builtins.formatting.phpcsfixer.with { args = { "--rules=@PhpCsFixer", "--no-interaction", "--quiet", "fix",
     "$FILENAME" } }
 }
 
--- -- set additional linters
+-- set additional linters
 local linters = require "lvim.lsp.null-ls.linters"
 linters.setup {
   { command = "codespell" },
@@ -241,17 +110,6 @@ lvim.plugins = {
       --   If not available, we use `mini` as the fallback
       -- "rcarriga/nvim-notify",
     }
-  },
-  -- Substitute and exchange operations (keys defined at the top)
-  {
-    "gbprod/substitute.nvim",
-    config = function()
-      require("substitute").setup({
-        -- your configuration comes here
-        -- or leave it empty to use the default settings
-        -- refer to the configuration section below
-      })
-    end,
   },
   -- Press tab in insert mode to escape from "" or {}
   {
@@ -354,22 +212,6 @@ lvim.plugins = {
       require('telescope').load_extension('neoclip')
     end,
   },
-  -- Pretty list of diagnostics, errors, etc.
-  {
-    "folke/trouble.nvim",
-    cmd = "TroubleToggle",
-  },
-  -- Preview / jump to :XX
-  -- {
-  --   "nacro90/numb.nvim",
-  --   event = "BufRead",
-  --   config = function()
-  --     require("numb").setup {
-  --       show_numbers = true,
-  --       show_cursorline = true,
-  --     }
-  --   end,
-  -- },
   -- Highlight first letters for easier motion
   {
     'jinh0/eyeliner.nvim',
@@ -384,34 +226,14 @@ lvim.plugins = {
     "ggandor/lightspeed.nvim",
     event = "BufRead",
   },
-  -- Testing framework
+  -- Python test debugging
   {
-    "nvim-neotest/neotest",
-    requires = {
-      "nvim-neotest/neotest-python",
-      "nvim-lua/plenary.nvim",
-      "nvim-treesitter/nvim-treesitter",
-      "antoinemadec/FixCursorHold.nvim"
-    },
+    "mfussenegger/nvim-dap-python",
     config = function()
-      require("neotest").setup({
-        adapters = {
-          require("neotest-python")({
-            dap = { justMyCode = false },
-          }),
-        },
-        quickfix = { enabled = true, open = false },
-      })
+      require('dap-python').setup()
+      require('dap-python').test_runner = 'pytest'
     end,
   },
-  -- Show function signature while you type
-  -- {
-  --   "ray-x/lsp_signature.nvim",
-  --   event = "BufRead",
-  --   config = function()
-  --     require "lsp_signature".on_attach()
-  --   end,
-  -- },
   -- When opening a file, put cursor at the last place it was
   {
     "ethanholz/nvim-lastplace",
@@ -435,21 +257,6 @@ lvim.plugins = {
     end,
   },
 }
--- DAP setup
-local dap = require("dap")
-dap.adapters.python = {
-  type = 'executable';
-  command = '.venv/bin/python';
-  args = { '-m', 'debugpy.adapter' };
-}
-dap.configurations.python = {}
--- local dapui = require("dapui")
--- dap.listeners.before.event_terminated["dapui_config"] = function()
---   dapui.close()
--- end
--- dap.listeners.before.event_exited["dapui_config"] = function()
---   dapui.close()
--- end
 
 -- Autocommands (https://neovim.io/doc/user/autocmd.html)
 vim.api.nvim_create_autocmd("BufEnter", {
